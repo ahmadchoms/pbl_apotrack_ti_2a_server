815eb3ce59537125ffc12a4d9f21dd2a457307de
ahmad chomsin <ahmadruddin17@gmail.com>
2026-05-20 14:28:29 +0700

    update

--- app/Services/Api/PharmacyService.php
+++ app/Services/Api/PharmacyService.php
@@ -14,7 +14,7 @@ class PharmacyService
     {
         $query = Pharmacy::query()
             ->select(['id', 'name', 'address', 'latitude', 'longitude', 'rating', 'verification_status', 'is_active'])
-            ->with(['operatingHours' => function($q) {
+            ->with(['operatingHours' => function ($q) {
                 $q->select(['id', 'pharmacy_id', 'day_of_week', 'open_time', 'close_time', 'is_closed']);
             }])
             ->where('verification_status', 'VERIFIED')
@@ -29,7 +29,7 @@ class PharmacyService
         if (isset($filters['latitude']) && isset($filters['longitude'])) {
             $lat = $filters['latitude'];
             $lng = $filters['longitude'];
-            
+
             $query->selectRaw(
                 "( 6371 * acos( cos( radians(?) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(?) ) + sin( radians(?) ) * sin( radians( latitude ) ) ) ) AS distance",
                 [$lat, $lng, $lat]
@@ -46,9 +46,8 @@ class PharmacyService
      */
     public function getPharmacyDetail($id)
     {
-        return Pharmacy::select(['id', 'name', 'address', 'phone', 'latitude', 'longitude', 'rating', 'total_reviews'])
+        return Pharmacy::select(['id', 'name', 'address', 'phone', 'logo_url', 'latitude', 'longitude', 'rating', 'total_reviews'])
             ->with([
-                'images:id,pharmacy_id,image_url,is_primary',
                 'operatingHours',
                 'legality:id,pharmacy_id,sia_number'
             ])
